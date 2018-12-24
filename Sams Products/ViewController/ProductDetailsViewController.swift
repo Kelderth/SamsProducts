cac0a8c4f1e9b56cd392345748f9dd9b511cbb84
Kelderth <Kelderth@gmail.com>
2018-12-24 17:24:05 -0500

    Product Detail View Controller was added

---
+++ Sams Products/ViewController/ProductDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProductDetailsViewController.swift
+//  Sams Products
+//
+//  Created by MCS on 12/24/18.
+//  Copyright © 2018 Kelderth. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
